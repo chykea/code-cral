0e796c300fe7173edaca2a11c5d9164acb931134
chykea <78532156@qq.com>
2023-08-25 18:05:04 +0800

    代码随想录 回溯

---
+++ 43.子集.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function (nums) {
+    const res = [[]];
+    const path = [];
+    const dfs = (n) => {
+        res.push([...path])
+        for (let i = n; i < nums.length; i++) {
+            path.push(nums[i])
+            dfs(i + 1);
+            path.pop()
+        }
+    }
+    dfs(0)
+    return res
+};
+console.log(subsets([1, 2, 3]));
